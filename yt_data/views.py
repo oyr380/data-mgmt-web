c88dc3c1bbd112acba6ae45ed6df726111c8231a
daniel-j-castro <74943988+daniel-j-castro@users.noreply.github.com>
2022-05-19 17:41:18 -0500

    Update views.py

--- yt_data/views.py
+++ yt_data/views.py
@@ -11,7 +11,6 @@ def home(request):
     if request.method == 'POST':
         query = request.POST['search']
         yt_channels = []
-       # query = '^'+query
         channels = instance.search_for_channels(query)
         if channels != None:
             for channel in channels:
@@ -47,14 +46,12 @@ def channel(request, chname=''):
         channel = instance.grab_one_channel(chname)
         if page:
             if int(page) == 1 or 1 > int(page) or total_pages < int(page):
-                #videos = ["https://www.youtube.com/watch?v=X-TkrWpO75k&ab_channel=EminemVEVO","https://www.youtube.com/watch?v=X-TkrWpO75k&ab_channel=EminemVEVO","https://www.youtube.com/watch?v=X-TkrWpO75k&ab_channel=EminemVEVO", "https://www.youtube.com/watch?v=X-TkrWpO75k&ab_channel=EminemVEVO","https://www.youtube.com/watch?v=X-TkrWpO75k&ab_channel=EminemVEVO","https://www.youtube.com/watch?v=X-TkrWpO75k&ab_channel=EminemVEVO","https://www.youtube.com/watch?v=X-TkrWpO75k&ab_channel=EminemVEVO","https://www.youtube.com/watch?v=X-TkrWpO75k&ab_channel=EminemVEVO","https://www.youtube.com/watch?v=X-TkrWpO75k&ab_channel=EminemVEVO","https://www.youtube.com/watch?v=X-TkrWpO75k&ab_channel=EminemVEVO"] 
                 page = 1
                 skip = 50 * (page - 1)
                 videos = instance.grab_channel_videos(chname, skip)
             else:
                 skip = 50 * (int(page) - 1)
                 videos = instance.grab_channel_videos(chname, skip)
-            #query based on pagination
         else:
             page = 1
             skip = 50 * (page - 1)
@@ -62,7 +59,6 @@ def channel(request, chname=''):
             videos = instance.grab_channel_videos(chname, skip)
             end = time.time()
             print(end-start)
-            #default query (find().limit(10))
     context = {
         'chname' : chname,
         'videos' : videos,
