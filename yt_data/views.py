44c0c4b8245c482b0de4056e875c01512e538dde
daniel-j-castro <74943988+daniel-j-castro@users.noreply.github.com>
2022-05-19 17:39:30 -0500

    Update views.py

--- yt_data/views.py
+++ yt_data/views.py
@@ -10,8 +10,7 @@ def home(request):
 
     if request.method == 'POST':
         query = request.POST['search']
-        yt_channels = []
-       # query = '^'+query
+        yt_channels = []       
         channels = instance.search_for_channels(query)
         if channels != None:
             for channel in channels:
@@ -45,19 +44,16 @@ def channel(request, chname=''):
         channel = instance.grab_one_channel(chname)
         if page:
             if int(page) == 1 or 1 > int(page) or total_pages < int(page):
-                #videos = ["https://www.youtube.com/watch?v=X-TkrWpO75k&ab_channel=EminemVEVO","https://www.youtube.com/watch?v=X-TkrWpO75k&ab_channel=EminemVEVO","https://www.youtube.com/watch?v=X-TkrWpO75k&ab_channel=EminemVEVO", "https://www.youtube.com/watch?v=X-TkrWpO75k&ab_channel=EminemVEVO","https://www.youtube.com/watch?v=X-TkrWpO75k&ab_channel=EminemVEVO","https://www.youtube.com/watch?v=X-TkrWpO75k&ab_channel=EminemVEVO","https://www.youtube.com/watch?v=X-TkrWpO75k&ab_channel=EminemVEVO","https://www.youtube.com/watch?v=X-TkrWpO75k&ab_channel=EminemVEVO","https://www.youtube.com/watch?v=X-TkrWpO75k&ab_channel=EminemVEVO","https://www.youtube.com/watch?v=X-TkrWpO75k&ab_channel=EminemVEVO"] 
                 page = 1
                 skip = 50 * (page - 1)
                 videos = instance.grab_channel_videos(chname, skip)
             else:
                 skip = 50 * (int(page) - 1)
                 videos = instance.grab_channel_videos(chname, skip)
-            #query based on pagination
         else:
             page = 1
             skip = 50 * (page - 1)
             videos = instance.grab_channel_videos(chname, skip)
-            #default query (find().limit(10))
     context = {
         'chname' : chname,
         'videos' : videos,
